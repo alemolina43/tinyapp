9368ca7c71abba4915c712c1bc7f28474853b3de
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-04 19:23:11 -0800

    Modify _header to display username using cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+let cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs"); //set EJS as the view engine
 
@@ -41,10 +43,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls"); // Redirect back to the urls page
 });
 
+//update an existing URL
 app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   const newLongURL = req.body.longURL;
-  urlDatabase[id] = newLongURL; // Update the URL in the database
+  urlDatabase[id] = newLongURL;
   res.redirect("/urls"); // Redirect back to the URLs page
 });
 
@@ -67,23 +70,35 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  //console.log('Cookies: ', req.cookies);
+  //Cookies:  { username: 'vanillaice' }
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const longURL = urlDatabase[req.params.id];
-  const templateVars = { id, longURL };
+  const templateVars = {
+    id,
+    longURL: urlDatabase[id],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
+  //const username = req.cookies["username"];
   res.redirect(longURL);
 });
 
